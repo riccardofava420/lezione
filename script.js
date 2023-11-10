e747850569abb2a45ccea2faf0d9e3c7aa572176
riccardofava420 <147819976+riccardofava420@users.noreply.github.com>
2023-11-10 13:51:34 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+alert("hey hey hey coal shutup");
+/**
+ * commento multiriga
+ * eta e una variabile
+ * la variabile occupa memoria
+ * let/keyboard
+ * camel case
+ */
+let eta =window.prompt("quanti anni hai?");
+if(eta = 17){
+    alert("eddai eddai, pari giorgia sotto steroidi!")
+    }
+    if(eta < 16){
+alert("mi dispiace ma non puoi accedere alla pagina!")
+}if(eta>18){
+    alert("benvenuto");
+}
+let matricola = 100; //tipo di dato, numero
+let saluto = "ciao il mio nome chiamo dario"; //tipo di dato boulean
+let luce = false
+
+console.log(saluto)
+console.log("la mia matricola e:" + matricola);
+console.log("luce" + luce);
+
+//stamperanno in console in tre colori
+//uniamo i valori per 'stamp'
+ let a = 3
+ let b = 5
+ console.log("la somma vale" + a + b);
+ console.log(a + b)
+ console.log("la somma vale" + "somma")
